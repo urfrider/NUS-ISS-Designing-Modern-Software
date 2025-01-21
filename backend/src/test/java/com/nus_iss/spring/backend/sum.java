333e1af3ac9a36b10c0080603afdeeb5c89f7431
urfrider <wnsl9704@naver.com>
2025-01-21 13:21:23 +0800

    update jenkins pipeline

---
+++ backend/src/test/java/com/nus_iss/spring/backend/sum.java
@@ -0,0 +1,19 @@
+package com.nus_iss.spring.backend;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.jayway.jsonpath.internal.function.numeric.Sum;
+
+public class sum {
+
+    @Test
+    public void testAdd() {
+        int a = 5;
+        int b = 3;
+        assertEquals(8, a + b, "5 + 3 should equal 5");
+        System.out.println("TEST SUCCESS!");
+    }
+
+}
