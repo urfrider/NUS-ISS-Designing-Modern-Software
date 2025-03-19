198d7616f3ededaad33e6a968e1d56afc60a03d9
urfrider <wnsl9704@naver.com>
2025-03-19 14:39:32 +0800

    hotfix

--- backend/src/main/java/com/nus_iss/spring/backend/entities/Order.java
+++ backend/src/main/java/com/nus_iss/spring/backend/entities/Order.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
