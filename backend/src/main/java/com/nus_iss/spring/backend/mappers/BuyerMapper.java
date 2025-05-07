7a3bb8a32a70f3943e2e9d45a383395e22929822
kecheng <guo.kecheng99@gmail.com>
2025-05-07 17:47:56 +0800

    Add top up button

--- backend/src/main/java/com/nus_iss/spring/backend/mappers/BuyerMapper.java
+++ backend/src/main/java/com/nus_iss/spring/backend/mappers/BuyerMapper.java
@@ -1,12 +1,13 @@
 package com.nus_iss.spring.backend.mappers;
 
+import org.springframework.stereotype.Component;
+
 import com.nus_iss.spring.backend.dtos.BuyerDto;
 import com.nus_iss.spring.backend.dtos.CartDto;
 import com.nus_iss.spring.backend.entities.Buyer;
 
+@Component
 public class BuyerMapper {
-
-    static CartMapper cartMapper;
 
     public static BuyerDto toDto(Buyer buyer) {
         if (buyer == null) {
@@ -17,9 +18,15 @@ public class BuyerMapper {
         buyerDTO.setId(buyer.getId());
         buyerDTO.setUsername(buyer.getUsername());
         buyerDTO.setAddress(buyer.getAddress());
+        buyerDTO.setBalance(buyer.getBalance());
 
-        CartDto cartDto = cartMapper.toDto(buyer.getCart());
-        buyerDTO.setCart(cartDto);
+        if (buyer.getCart() != null) {
+            CartDto cartDto = CartMapper.toDto(buyer.getCart());
+
+            buyerDTO.setCart(cartDto);
+        } else {
+            buyerDTO.setCart(null);
+        }
 
         return buyerDTO;
     }
--- frontend/src/redux/userSlice.ts
+++ frontend/src/redux/userSlice.ts
@@ -52,7 +52,7 @@ const userSlice = createSlice({
       if (updatedUser.address !== undefined)
         state.address = updatedUser.address;
       if (updatedUser.uen !== undefined) state.uen = updatedUser.uen;
-
+      if (updatedUser.balance !== undefined) state.balance = updatedUser.balance;
       // Update local storage so the changes persist
       localStorage.setItem("user", JSON.stringify(state));
     },
